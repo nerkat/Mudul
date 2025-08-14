9f0cd30961080b0dd317b5be9be132a68eda9643
KingKatz <nerkat@gmail.com>
2025-08-14 14:07:25 +0000

    bootstrap preline

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin';
+
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+    // Preline components
+    'node_modules/preline/dist/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [preline],
+};
