3616ad01a40ccbb4b6adcd2feb6cf41bf6461b43
KingKatz <nerkat@gmail.com>
2025-08-14 14:39:51 +0000

    clean

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import preline from 'preline/plugin';
 
 export default {
   content: [
     './index.html',
-    './src/**/*.{ts,tsx}',
-    // Preline components
-    'node_modules/preline/dist/*.js',
+    './src/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {},
-  },
-  plugins: [preline],
+  }
 };
