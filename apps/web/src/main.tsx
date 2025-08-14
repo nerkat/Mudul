b36f5c2429cf2d388d83f55c49e3648e89817b43
KingKatz <nerkat@gmail.com>
2025-08-14 14:39:51 +0000

    clean

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import 'preline';
 import { NodePage } from "./routes/NodePage";
 import { ViewModeProvider } from "./ctx/ViewModeContext";
 
